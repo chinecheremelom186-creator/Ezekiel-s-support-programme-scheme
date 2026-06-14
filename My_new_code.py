fdac1dfaeda05ad2046bf38391230a1ba2a89c59
chinecheremelom186-creator <chinecheremelom186@gmail.com>
2026-06-14 22:24:00 +0100

    Create My_new_code.py

---
+++ My_new_code.py
@@ -0,0 +1,31 @@
+print("=========================")
+print("SCHOLARSHIP APPLICATION")
+
+print("=========================")
+age = int(input("\nEnter your age :\t"))
+
+if age >=18 :
+    print("age requirement met . you can continue")
+
+    name = input("Enter your full name :\t")
+    school = input("\nEnter your school :\t")
+    birth = input("\nplace of birth :\t")
+    nationality = input("\nEnter your nationality :\t")
+
+
+    print("\n========================")
+    print(" APPLICATION COMPLETED")
+    print("========================")
+
+    print("\nname", name)
+    print("\nschool", school)
+    print("\nbirth", birth)
+    print("\nnationality:", nationality)
+
+else :
+            print("\nage requirement not met")
+            print("sorry oo 💔💔,😂 no be my fault 🫴 .")
+            print("\nsend 3k to 8061941988")
+            print("opay")
+            print("\nTHANKS FOR BANKING WITH ME")
+            
